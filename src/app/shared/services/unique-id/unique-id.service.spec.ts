894ec2b4e3fbcbad1b95dedd6924f9c8a8150098
saviolopes018 <savio.lopes404@gmail.com>
2022-11-10 23:56:35 -0300

    implementation of good practices

--- src/app/shared/services/unique-id/unique-id.service.spec.ts
+++ src/app/shared/services/unique-id/unique-id.service.spec.ts
@@ -1,8 +1,8 @@
 import { UniqueIdService } from "./unique-id.service";
 
-describe('UniqueIdService', () => {
+describe(UniqueIdService.name, () => {
 
-    it('#generateUniqueIdWithPrefix should generate id when called with prefix', () => {
+    it(`#${UniqueIdService.prototype.generateUniqueIdWithPrefix.name} should generate id when called with prefix`, () => {
         const service = new UniqueIdService();
         const id = service.generateUniqueIdWithPrefix('app');
         expect(id).toContain('app-');
