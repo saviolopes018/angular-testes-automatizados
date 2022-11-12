28f82759519c5c43c4f520e3f0855e5c8d6de308
saviolopes018 <savio.lopes404@gmail.com>
2022-11-12 00:37:36 -0300

    implementing before each

--- src/app/shared/services/unique-id/unique-id.service.spec.ts
+++ src/app/shared/services/unique-id/unique-id.service.spec.ts
@@ -2,10 +2,28 @@ import { UniqueIdService } from "./unique-id.service";
 
 describe(UniqueIdService.name, () => {
 
+    let service: UniqueIdService = null;
+    beforeEach(() => {
+        service = new UniqueIdService();
+    });
+
     it(`#${UniqueIdService.prototype.generateUniqueIdWithPrefix.name} should generate id when called with prefix`, () => {
-        const service = new UniqueIdService();
-        const id = service.generateUniqueIdWithPrefix('app-');
+        const id = service.generateUniqueIdWithPrefix('app');
         expect(id.startsWith('app-')).toBeTrue();
+    });
+
+    it(`#${UniqueIdService.prototype.generateUniqueIdWithPrefix.name} should not generate duplicate Ids when called multiple times`, () => {
+        const ids = new Set();
+        for(let i = 0; i < 50; i++){
+            ids.add(service.generateUniqueIdWithPrefix('app'));
+        }
+        expect(ids.size).toBe(50);
+    });
+
+    it(`#${UniqueIdService.prototype.getNumberOfGeneratedIds.name} should return the number of generatedIds when called`, () => {
+        service.generateUniqueIdWithPrefix('app');
+        service.generateUniqueIdWithPrefix('app');
+        expect(service.getNumberOfGeneratedIds()).toBe(2);
     });
 
 });
