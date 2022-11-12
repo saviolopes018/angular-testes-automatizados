598bbf91c854f2848bcd9990ebfbc8801f5e0dce
saviolopes018 <savio.lopes404@gmail.com>
2022-11-12 00:19:56 -0300

    improvements in test

--- src/app/shared/services/unique-id/unique-id.service.spec.ts
+++ src/app/shared/services/unique-id/unique-id.service.spec.ts
@@ -4,8 +4,8 @@ describe(UniqueIdService.name, () => {
 
     it(`#${UniqueIdService.prototype.generateUniqueIdWithPrefix.name} should generate id when called with prefix`, () => {
         const service = new UniqueIdService();
-        const id = service.generateUniqueIdWithPrefix('app');
-        expect(id).toContain('app-');
+        const id = service.generateUniqueIdWithPrefix('app-');
+        expect(id.startsWith('app-')).toBeTrue();
     });
 
 });
