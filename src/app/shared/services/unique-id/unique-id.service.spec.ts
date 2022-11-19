5d8ec3cd3ed8166b203e90344b5a86dca18a464a
saviolopes018 <savio.lopes404@gmail.com>
2022-11-19 12:47:51 -0300

    test with withContext

--- src/app/shared/services/unique-id/unique-id.service.spec.ts
+++ src/app/shared/services/unique-id/unique-id.service.spec.ts
@@ -27,9 +27,11 @@ describe(UniqueIdService.name, () => {
     });
 
     it(`#${UniqueIdService.prototype.generateUniqueIdWithPrefix.name} should throw when called with empty`, () => {
-        const emptyValues = [null, undefined, '', '0', '1'];
+        const emptyValues = [null, undefined, '', '0', '1', 'app'];
         emptyValues.forEach(emptyValue => {
-            expect(() => service.generateUniqueIdWithPrefix(emptyValue)).toThrow();
+            expect(() => service.generateUniqueIdWithPrefix(emptyValue))
+            .withContext(`Empty Value: ${emptyValue}`)
+            .toThrow();
         });
     });
 
