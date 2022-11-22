5dcc418b9a4580fdecfef9c6f7c5bdf4f7c87d5a
saviolopes018 <savio.lopes404@gmail.com>
2022-11-22 17:58:00 -0300

    Test with TestBed and ComponentFixture

--- src/app/shared/components/like-widget/like-widget.component.spec.ts
+++ src/app/shared/components/like-widget/like-widget.component.spec.ts
@@ -1,18 +1,25 @@
-import { TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { UniqueIdService } from '../../services/unique-id/unique-id.service';
 
 import { LikeWidgetComponent } from './like-widget.component';
 
 describe(LikeWidgetComponent.name, () => {
-  let component: LikeWidgetComponent = null;
+  let fixture: ComponentFixture<LikeWidgetComponent> = null;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [LikeWidgetComponent]
+      declarations: [LikeWidgetComponent],
+      providers: [UniqueIdService],
+      imports: [FontAwesomeModule]
     }).compileComponents();
+
+    fixture = TestBed.createComponent(LikeWidgetComponent);
   });
 
-  it('', () => {
-    
+  it('Should create component', () => {
+    const instance = fixture.componentInstance;
+    expect(instance).toBeTruthy();
   });
 
 });
